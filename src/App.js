a81bd2020bd555a60f92daa85abc542e8a3e5d3b
Henry <hmorenosifontes@gmail.com>
2023-02-27 08:19:46 -0400

    Cabios2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import {Router , Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
 
-
-
-function App() {
-  return (<div>
-    <Router>
-
-    <Route path='/' element={tv1}></Route>
-
-    </Router>
-   
-    </div>
-  );
-}
-
+const App = () => (
+  <Router>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Routes>
+  </Router>
+);
 export default App;
--- src/components/tv01/Tv01.jsx
+++ src/components/tv01/Tv01.jsx
@@ -1,5 +1,10 @@
-// import React from "react";
+import React from "react";
 
+const About = () => (
+  <h1>This is a PWA</h1>
+);
+
+export default About;
 
 
 
@@ -30,50 +35,49 @@
 
 
 
+// import React, { useEffect, useRef } from "react";
 
-import React, { useEffect, useRef } from "react";
 
+// export default function Tv01() {
+//   const videoEl = useRef(null);
 
-export default function Tv01() {
-  const videoEl = useRef(null);
+//   const attemptPlay = () => {
+//     videoEl &&
+//       videoEl.current &&
+//       videoEl.current.play().catch(error => {
+//         console.error("Error attempting to play", error);
+//       });
+//   };
 
-  const attemptPlay = () => {
-    videoEl &&
-      videoEl.current &&
-      videoEl.current.play().catch(error => {
-        console.error("Error attempting to play", error);
-      });
-  };
+//   useEffect(() => {
+//     attemptPlay();
+//   }, []);
 
-  useEffect(() => {
-    attemptPlay();
-  }, []);
-
-  return (
-    <div >
-      <h1>Autoplay example</h1>
-      <div>
-        <video
-          style={{ maxWidth: "100%", width: "900px", margin: "0 auto"}}
-          playsInline
-          loop
+//   return (
+//     <div >
+//       <h1>Autoplay example</h1>
+//       <div>
+//         <video
+//           style={{ maxWidth: "100%", width: "900px", margin: "0 auto"}}
+//           playsInline
+//           loop
           
-          controls
-          alt="All the devices"
-          // src="https://stream.mux.com/6fiGM5ChLz8T66ZZiuzk1KZuIKX8zJz00/medium.mp4"
-          // src="https://res.cloudinary.com/dsbemomaf/video/upload/v1676334365/nonvwa9spui6owdjxzpr.mp4"
-          src="https://res.cloudinary.com/dsbemomaf/video/upload/v1676334366/k9f8v7eqsdahftb2e2pv.mp4"
+//           controls
+//           alt="All the devices"
+//           // src="https://stream.mux.com/6fiGM5ChLz8T66ZZiuzk1KZuIKX8zJz00/medium.mp4"
+//           // src="https://res.cloudinary.com/dsbemomaf/video/upload/v1676334365/nonvwa9spui6owdjxzpr.mp4"
+//           src="https://res.cloudinary.com/dsbemomaf/video/upload/v1676334366/k9f8v7eqsdahftb2e2pv.mp4"
           
-          ref={videoEl}
+//           ref={videoEl}
 
           
-        />
+//         />
 
-   <div> <iframe width="300" height="300" src="https://www.youtube.com/embed/cPcLEGKR6ZI" title="TOP 5  ACCESORIOS DE REALIDAD VIRTUAL | TGN" frameborder="0" allow="accelerometer; clipboard-write;   encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="100%" ></iframe></div>
-      </div>
-    </div>
-  );  
-}
+//    <div> <iframe width="300" height="300" src="https://www.youtube.com/embed/cPcLEGKR6ZI" title="TOP 5  ACCESORIOS DE REALIDAD VIRTUAL | TGN" frameborder="0" allow="accelerometer; clipboard-write;   encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="100%" ></iframe></div>
+//       </div>
+//     </div>
+//   );  
+// }
 
 
 
